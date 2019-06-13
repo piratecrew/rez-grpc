1b59dfe5c176acd32ec64f4c3a7906f785bb29b8
Fredrik Brännbacka <fredrik.brannbacka@gmail.com>
2019-06-13 13:55:43 +0200

    Update package.py

--- package.py
+++ package.py
@@ -8,7 +8,9 @@ authors = [
 
 description = \
     """
-    FlatBuffers is a cross platform serialization library architected for maximum memory efficiency. It allows you to directly access serialized data without parsing/unpacking it first, while still having great forwards/backwards compatibility.
+    gRPC is a modern open source high performance RPC framework that can run in any environment. 
+    It can efficiently connect services in and across data centers with pluggable support for load balancing, tracing, health checking and authentication. 
+    It is also applicable in last mile of distributed computing to connect devices, mobile applications and browsers to backend services.
     """
 
 @early()
